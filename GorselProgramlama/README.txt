c506ad336d398f058a5cd884d25a7ac88bec0281
Gökhan Arda <gokhan28012arda@gmail.com>
2026-01-15 02:13:46 +0300

    Yedek sql bağlantsı eklendi

---
+++ GorselProgramlama/README.txt
@@ -0,0 +1,32 @@
+﻿1. PROJE HAKKINDA
+---------------------------------------------------------
+Bu proje, C# Windows Forms ve SQL Server kullanılarak geliştirilmiş bir Otopark Takip Sistemidir. 
+
+2. VERİTABANI KURULUMU VE BAĞLANTI AYARLARI 
+---------------------------------------------------------
+Projenin çalışması için "Gorsel" isimli bir veritabanına ihtiyaç duyulmaktadır. 
+
+YÖNTEM A (Varsayılan - LocalDB):
+Proje, "AttachDbFilename" yöntemiyle "Data/Gorsel.mdf" dosyasına bağlanacak şekilde ayarlanmıştır.
+
+YÖNTEM B (Yedek - SQL Script):
+Sürüm uyuşmazlığı nedeniyle .mdf dosyası açılmazsa, ana klasördeki "Veritabani_Kurulum.sql" dosyasını SSMS üzerinde çalıştırarak veritabanını sisteminize kurabilirsiniz.
+
+ÖNEMLİ NOT: Yöntem B'yi kullanırsanız, AuthService içerisindeki bağlantı adresini (Connection String) aşağıdaki gibi güncellemeniz gerekmektedir:
+
+MEVCUT KOD (MDF Odaklı):
+public static string _connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Data\Gorsel.mdf;Integrated Security=True;Connect Timeout=30";
+
+DEĞİŞTİRİLMESİ GEREKEN KOD (Script Kurulumu Sonrası):
+public static string _connString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Gorsel;Integrated Security=True;";
+
+3. SİSTEME GİRİŞ
+---------------------------------------------------------
+Veritabanı güvenlik ve temiz kurulum amacıyla "BOŞ" olarak yapılandırılmıştır. 
+Lütfen uygulamayı başlattıktan sonra "KAYIT OL" butonunu kullanarak yeni bir kullanıcı oluşturunuz.
+
+4. TEKNİK ÖZELLİKLER
+---------------------------------------------------------
+- Şifreleme: Kullanıcı şifreleri SHA-256 algoritması ile hashlenerek saklanır.
+- Ücretlendirme: İlk 15 dakika ücretsiz, sonrası saatlik tarife üzerinden hesaplanır.
+- Görsel Arayüz: Dolu/Boş park yerleri dinamik renkli butonlarla takip edilir.
---
+++ GorselProgramlama/Veritabani_Kurulum2.sql
@@ -0,0 +1,54 @@
+﻿USE [master]
+GO
+
+/****** 1. ADIM: Veritabanı Kontrolü ve Oluşturma ******/
+-- Veritabanı yoksa oluşturur, varsa hata vermeden geçer.
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Gorsel')
+BEGIN
+    CREATE DATABASE [Gorsel]
+END
+GO
+
+USE [Gorsel]
+GO
+
+/****** 2. ADIM: Kullanıcı Tablosunu Oluşturma (tblUser) ******/
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblUser]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[tblUser](
+        [ID] [int] IDENTITY(1,1) NOT NULL,
+        [Name] [nvarchar](50) NOT NULL,
+        [Surname] [nvarchar](50) NOT NULL,
+        [PlateNumber] [nvarchar](20) NOT NULL,
+        [Password] [nvarchar](200) NOT NULL,
+        CONSTRAINT [PK_tblUser] PRIMARY KEY CLUSTERED ([ID] ASC),
+        CONSTRAINT [UQ_PlateNumber] UNIQUE NONCLUSTERED ([PlateNumber] ASC)
+    )
+END
+GO
+
+/****** 3. ADIM: Park İşlemleri Tablosunu Oluşturma (tblParkIslemleri) ******/
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblParkIslemleri]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[tblParkIslemleri](
+        [ID] [int] IDENTITY(1,1) NOT NULL,
+        [KatNumarasi] [int] NOT NULL,
+        [ParkYeriNumarasi] [int] NOT NULL,
+        [UserID] [int] NOT NULL,
+        [GirisSaati] [datetime] NULL DEFAULT (getdate()),
+        [CikisSaati] [datetime] NULL,
+        [Durum] [bit] NULL DEFAULT ((1)),
+        CONSTRAINT [PK_tblParkIslemleri] PRIMARY KEY CLUSTERED ([ID] ASC)
+    )
+END
+GO
+
+/****** 4. ADIM: Tablolar Arası İlişkiler (Foreign Key) ******/
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_tblParkIslemleri_tblUser]'))
+BEGIN
+    ALTER TABLE [dbo].[tblParkIslemleri] WITH CHECK ADD CONSTRAINT [FK_tblParkIslemleri_tblUser] FOREIGN KEY([UserID])
+    REFERENCES [dbo].[tblUser] ([ID])
+    
+    ALTER TABLE [dbo].[tblParkIslemleri] CHECK CONSTRAINT [FK_tblParkIslemleri_tblUser]
+END
+GO
